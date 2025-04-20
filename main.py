0d457226854b27ae4d4edaa617900034e30a361e
Mohusse1 <mohamedhussein.js@gmail.com>
2025-04-19 23:07:47 -0400

    Added code block at top of main to set file directory at run, this should allow access to parent directory regardless of run point

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # main.py
+import sys
+import os
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
 from scheduler import calendar_api  # ✅ using module-level import for mocking
 
 def main():
